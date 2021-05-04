8a7966ad4c26cb203815ecc785b647a082b46278
Aaliyah <aaliyah.symlar@vanderbilt.edu>
2021-05-03 21:53:21 -0500

    added jwt. added new auth

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,18 +1,12 @@
  const { User, Thought } = require('../models');
+ const { AuthenticationError } = require('apollo-server-express');
+ const { signToken } = require('../utils/auth');
+const { sign } = require('jsonwebtoken');
 
 const resolvers = {
     Query: {
-        thoughts: async (parent, { username }) => {
-            const params = username ? { username } : {};
-            return Thought.find(params).sort({ createdAt: -1 });
-          },
-          // get one thought
-          thought: async (parent, { _id }) => {
-              return Thought.findOne({ _id});
-          },
-
-          // get all users
-          user: async (parent, { username }) => {
+         // get all users
+         user: async (parent, { username }) => {
             return User.findOne({ username })
               .select('-__v -password')
               .populate('friends')
@@ -24,8 +18,44 @@ const resolvers = {
               .select('-__v -password')
               .populate('friends')
               .populate('thoughts');
-          }
+          },
+
+        thoughts: async (parent, { username }) => {
+            const params = username ? { username } : {};
+            return Thought.find(params).sort({ createdAt: -1 });
+          },
+          // get one thought
+          thought: async (parent, { _id }) => {
+              return Thought.findOne({ _id});
+          },
+
+         
     },
+
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+          
+            if (!user) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+          
+            const correctPw = await user.isCorrectPassword(password);
+          
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+          }
+      }
   };
   
   module.exports = resolvers;
