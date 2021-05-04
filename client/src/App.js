49c65c4bb4a7a13e6b8e911b22dfc670c393af31
Aaliyah <aaliyah.symlar@vanderbilt.edu>
2021-05-04 17:56:35 -0500

    added profile and thought pages. added link method for nav.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 //add these two library import statements
-
 //special type of React component that we'll use to provide data to all of the other components
 
 import { ApolloProvider } from '@apollo/react-hooks';
@@ -11,8 +10,15 @@ import ApolloClient from 'apollo-boost';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
-
 import Home from './pages/Home';
+
+
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+
 
 const client = new ApolloClient({
   uri: '/graphql'
@@ -21,13 +27,23 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-    <div className='flex-column justify-flex-start min-100-vh'>
+      <Router>
+      <div className='flex-column justify-flex-start min-100-vh'>
       <Header />
       <div className='container'>
-        <Home />
+        <Switch>
+         <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/profile/:username?" component={Profile} />
+        <Route exact path="/thought/:id" component={SingleThought} />
+
+        <Route component={NoMatch} />
+        </Switch>
       </div>
       <Footer />
     </div>
+      </Router>
     </ApolloProvider>
   );
 }
